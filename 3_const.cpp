6c0ae811c32458c14d24f98aa494ae4bf3133d86
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-09-23 19:49:19 +0300

    3_const

---
+++ 3_const.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// https://www.youtube.com/watch?v=MwQEaCsS6UM&list=PLZPZq0r_RZOMHoXIcxze_lP97j2Ase2on&index=3
+
+int main() {
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    std::cout << circumference << "cm" << "\n";
+
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+    
+    return 0;
+}
